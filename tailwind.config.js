b80b35827822ad9679193228cf268c1fcb624c52
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-03-20 21:01:07 -0400

    feat: inicio filtros

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        marbold: ['Marbold', 'sans-serif'],
+      },
       colors: {
         'custom-green': '#E0F4B3',
         'custom-back': '#232323',
+        'custom-olive-green': '#738252',
+        'custom-green-other': '#CDEA98',
+        'custom-green-dark': '#A7C879',
+        'custom-text-green': '#738252',
       },
     },
   },
