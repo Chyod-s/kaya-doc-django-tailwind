7324f0046b17cafe26a688c6b62ed6d6c0166944
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-03-25 20:36:05 -0400

    fix: tentativa de correção botão e add pading

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,22 @@
-window.addEventListener('scroll', () => {
-    const header = document.getElementById('header');
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.getElementById('header');
+  const teste = document.getElementById('btn-teste');
+  const login = document.getElementById('btn-login');
+
+  window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
-      header.classList.add('bg-[#232323]/70', 'backdrop-blur-lg');
       header.classList.remove('bg-[#232323]');
-    } else {
-      header.classList.add('bg-[#232323]');
-      header.classList.remove('bg-[#232323]/70', 'backdrop-blur-lg');
+      header.classList.add('bg-[#232323]/70', 'backdrop-blur-lg');
+
+      teste.classList.remove('bg-custom-green');
+      teste.classList.remove('hover:bg-custom-back');
+      teste.classList.add('bg-custom-green-70', 'backdrop-blur-lg');
+      teste.classList.add('hover:bg-custom-back-70', 'backdrop-blur-lg');
+
+      login.classList.remove('bg-custom-back');
+      login.classList.remove('hover:bg-custom-green');
+      login.classList.add('bg-custom-back-70', 'backdrop-blur-lg');
+      login.classList.add('hover:bg-custom-green-70', 'backdrop-blur-lg');
     }
   });
-  
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,18 @@ module.exports = {
       },
       colors: {
         'custom-green': '#E0F4B3',
+        'custom-green-70': 'rgba(224, 244, 179, 0.7)',
         'custom-back': '#232323',
+        'custom-back-70': 'rgba(35, 35, 35, 0.7)',
         'custom-olive-green': '#738252',
         'custom-green-other': '#CDEA98',
         'custom-green-dark': '#A7C879',
         'custom-text-green': '#738252',
         'custom-text-grey': '#758554',
         'custon-cards-green': '#738252',
+      },
+      backdropBlur: {
+        lg: '10px',
       },
     },
   },
